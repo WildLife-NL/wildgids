1fd3215a9bd47d9544fa88badd13a2267d609b41
Cretu Claudia <523382@student.fontys.nl>
2025-10-09 13:48:08 +0200

    added widgets

--- wild_gids/lib/constants/app_text_theme.dart
+++ wild_gids/lib/constants/app_text_theme.dart
@@ -1,18 +1,22 @@
 import 'package:flutter/material.dart';
-import 'package:wild_gids/constants/app_colors.dart';
+import 'package:widgets/constants/app_colors.dart';
+
 
 class AppTextTheme {
   static final TextTheme textTheme = TextTheme(
     titleLarge: TextStyle(
       fontSize: 24,
       fontWeight: FontWeight.bold,
-      color: AppColors.dark,
+      color: AppColors.darkGreen, // Ensure this color exists
     ),
     titleMedium: TextStyle(
       fontSize: 20,
       fontWeight: FontWeight.w500,
-      color: AppColors.dark,
+      color: AppColors.darkGreen,
     ),
-    bodyMedium: TextStyle(fontSize: 16, color: AppColors.dark),
+    bodyMedium: TextStyle(
+      fontSize: 16,
+      color: AppColors.darkGreen,
+    ),
   );
 }
--- wild_gids/test/widget_test.dart
+++ wild_gids/test/widget_test.dart
@@ -7,8 +7,9 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:widgets/main.dart';
 
-import 'package:wild_gids/main.dart';
+
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
