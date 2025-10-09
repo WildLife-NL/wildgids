dbaeb80e7d10a4a156466a9dc61c9274d695d217
Cretu Claudia <523382@student.fontys.nl>
2025-10-09 15:50:21 +0200

    added api config

---
+++ wild_gids/lib/config/api_config.dart
@@ -0,0 +1,5 @@
+class ApiConfig {
+  static const String baseUrl = 'https://test-api-wildlifenl.uu.nl/reports/';
+  static const String authEndpoint = '$baseUrl/auth';
+  static const String reportEndpoint = '$baseUrl/reports'; 
+}
